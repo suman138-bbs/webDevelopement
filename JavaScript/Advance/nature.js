7b8814bbe964798c83172f81d5202f375b09dc75
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-07-17 18:29:19 +0530

    Higher Order Function

---
+++ JavaScript/Advance/nature.js
@@ -0,0 +1,59 @@
+
+
+// const a = {
+//     name: "Suman",
+//     password : 1234
+// }
+
+
+// const b = { ...a }
+// b.name = "Bhandari";
+// console.log(a)
+// console.log(b)
+
+
+// console.log(1 === '1');
+
+
+
+// function fun() {
+//     return function fun2(){console.log("Bye")}
+// }
+
+// fun()()
+
+
+
+function pritnMyname(name) {
+    console.log(name)
+}
+
+
+// function printMysirName(sirNAme) {
+//     console.log(sirNAme)
+// }
+
+
+// const HOF = (nameOrSirName,fun)=>{
+//         return fun(nameOrSirName)
+// }
+
+// HOF("Bhandari", printMysirName);
+// function MultiplyByTwo(number){
+//     return number * 2;
+// }
+
+// function MultiplyByTen(number){
+//     return number * 10;
+// }
+//  const MultiplyBy  = (num,fun)=>{
+//      console.log(fun(num))
+// }
+
+// MultiplyBy(10,MultiplyByTen)
+// MultiplyBy(10,MultiplyByTwo)
+
+
+ MultiplyBy = (num1)=>(num2)=>console.log(num1*num2)
+
+MultiplyBy(2)(3)
